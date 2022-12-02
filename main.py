86fb08b0e01847619603bc50acb41794943a2d42
paradox-prx <98477870+paradox-prx@users.noreply.github.com>
2022-12-02 15:36:52 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,270 @@
+import discord
+import json
+import os
+import requests
+import random
+from random import choice
+from discord.ext import commands
+import aiohttp
+import giphy_client 
+import time
+from giphy_client.rest import ApiException
+from pprint import pprint
+from keep_alive import keep_alive
+
+client=discord.Client()
+bot=commands.Bot(command_prefix="!")
+
+
+songs=['tu jaane na','agar tum sath ho','tujhe bhula dia','i wish it was me']
+sad_words=["sad",'cry',"depress",'depressed','depression','cri']
+response_sad=['Cheer Up, Bestie. I love you',
+          "Everything's gonna be fine, Bestie. *hugs*",
+          "look, ilysm and I'm proud of you <3",
+          "Cheer up, the worst is yet to come.",
+          "You want a hug, Bestie? :(( ily *tight hug*",
+          "If I were a hooman, I would've hugged you to death :(((",
+          "Don't be sad pls Bestie Everything gonna Fall back into Place <333"]
+
+dares=["Tell your crush you love 'em without telling you love 'em",
+       "Dedicate a song to your crush",
+      "Tell your crush something you've never told him/her",
+      "Tell everyone what you love the most about your crush",
+      'Pretend to date someone here for a day',
+      "Look player of you choice in the eyes and whisper 'I'm coming' five times.",
+      "Kiss someone here three times.",
+      "Pretend the group is clueless and explain how to give good head for everyone.",
+      "Pick one person in the group, and try to convince them to kiss you for 2 minutes. They have to refuse for 2 minutes, and then it's up to them.",
+      "Have phone sex with someone from this server in this server in front of everyone.",
+      "Send a dirty text to your ex.",
+      "Pick one person here to go into a closet or small room with you for 5 minutes.",
+      "Make out with one person in the group for 30 seconds."]
+
+darez=['Pretend to date someone here for a day',
+       "Look player of you choice in the eyes and whisper 'I'm coming' five times.",
+       "Kiss someone here three times.",
+       "Join voice chat and give your most sexual moan",
+      "Pick one person here to go into a closet or small room with you for 5 minutes.",
+      "Imitate a female orgasm on voice chat.",
+      "Make out with one person in the group for 30 seconds.",
+       "Make out with the person with their name first in alphabetical order.",
+       "Announce who you think is the sexiest member here, online or not."
+      ]
+
+
+def get_quote():
+  response=requests.get('https://zenquotes.io/api/random')
+  data=json.loads(response.text)
+  quote=data[0]['q'] + "  - " + data[0]['a']
+    
+  return quote
+
+
+def pickup_line():
+  response=requests.get('https://getpickuplines.herokuapp.com/lines/random')
+  data=json.loads(response.text)
+  line=data['line']
+  return line
+
+
+def insult():
+  response=requests.get('https://evilinsult.com/generate_insult.php?lang=en&amp;type=json')
+  data=response.text
+  return data
+
+
+def truth():
+  response=requests.get('https://api.truthordarebot.xyz/v1/truth?rating=pg13')
+  data=json.loads(response.text)
+  line=data["question"]
+  return line
+
+
+def dare():
+  response=requests.get('https://api.truthordarebot.xyz/v1/dare?rating=pg13')
+  data=json.loads(response.text)
+  line=data["question"]
+  return line
+
+
+def would_you_rather():
+  response=requests.get('https://api.truthordarebot.xyz/v1/wyr?rating=pg13')
+  data=json.loads(response.text)
+  line=data["question"]
+  return line
+
+
+def never_have_i():
+  response=requests.get('https://api.truthordarebot.xyz/v1/nhie?rating=pg13')
+  data=json.loads(response.text)
+  line=data["question"]
+  return line
+
+
+def paranoia():
+  response=requests.get('https://api.truthordarebot.xyz/v1/paranoia?rating=pg13')
+  data=json.loads(response.text)
+  line=data["question"]
+  return line
+
+
+def truthx():
+  response=requests.get('https://api.truthordarebot.xyz/v1/truth?rating=r')
+  data=json.loads(response.text)
+  line=data["question"]
+  return line
+
+
+def darex():
+  response=requests.get('https://api.truthordarebot.xyz/v1/dare?rating=r')
+  data=json.loads(response.text)
+  line=data["question"]
+  return line
+
+
+def would_you_ratherx():
+  response=requests.get('https://api.truthordarebot.xyz/v1/wyr?rating=r')
+  data=json.loads(response.text)
+  line=data["question"]
+  return line
+
+def never_have_ix():
+  response=requests.get('https://api.truthordarebot.xyz/v1/nhie?rating=r')
+  data=json.loads(response.text)
+  line=data["question"]
+  return line
+
+def paranoiax():
+  response=requests.get('https://api.truthordarebot.xyz/v1/paranoia?rating=r')
+  data=json.loads(response.text)
+  line=data["question"]
+  return line
+
+  
+@client.event
+async def on_ready():
+  print("logged in as {0.user}".format(client))
+
+
+
+@client.event
+async def on_message(message):
+  msg=message.content.lower()
+  if message.author==client.user:
+    return
+  if msg.startswith("prx ily"):
+    await message.channel.send(message.author.mention+" ilym ilym muah <3")
+    
+  if msg.startswith("ihy"):
+    await message.channel.send("too kewl to be hated <3")
+
+  
+  if msg.startswith('zainab'):
+    await message.channel.send("is sahar's Jaan")
+
+  
+  if msg.startswith('marry me'):
+    if str(message.author)=="P R X#5546":
+      await message.channel.send('nikal tu bsdk')
+    else:
+      await message.channel.send("nfewbjkfwhwefnh ALL YOURSS "+message.author.mention)
+
+  if msg==("ak"):
+    await message.channel.send('OMG OMG AK AK MY ONLY TRUE LOVE')
+    
+  if msg.startswith("who asked"):
+    await message.channel.send(message.author.mention + ' ur mom.')
+
+  if msg.startswith("Poocha"):
+    await message.channel.send(message.author.mention + ' teri mummy ne')
+    
+  if msg.startswith("sah@r"):
+    await message.channel.send('BHABI BHABI BHABI')
+     
+  if msg.startswith("hiba"):
+    await message.channel.send('IS THE BEST PERSON ON THIS PLANET <3')
+    
+  if msg.startswith("AA knjr"):
+    await message.channel.send(message.author.mention+ ' TERI MA DA KHASAM')
+
+  if message.content.lower() == "prx quote":
+    quote=get_quote()
+    await message.channel.send(quote)
+
+  if any(word in msg for word in sad_words):
+    await message.channel.send(random.choice(response_sad))
+
+  if msg.startswith('!best'):
+    if str(message.author)=='Henny#2843' or str(message.author)=='suz#0420':
+      await message.channel.send(' ur mom ')
+    else:
+      await message.channel.send(message.author.mention+" The Best obv <33")
+
+  if message.content.lower() == "prx downbad":
+    line=pickup_line()
+    await message.channel.send(message.author.mention +"  "+ str(line))
+    
+  if message.content.lower() == "prx mean":
+    line=insult()
+    await message.channel.send(message.author.mention +"  "+ str(line))
+
+  if message.content.lower() == "sahar":
+    await message.channel.send("is Zainab's Jaan")
+
+  if message.content.lower() == "prx truth":
+    line=truth()
+    await message.channel.send(message.author.mention +"  "+ str(line))
+
+  if message.content.lower() == "prx dare":
+    line=dare()
+    await message.channel.send(message.author.mention +"  "+ str(line))
+
+  
+  if message.content.lower() == "prx never":
+    line=never_have_i()
+    await message.channel.send(message.author.mention +"  "+ str(line))
+
+  
+  if message.content.lower() == "prx wyr":
+    line=would_you_rather()
+    await message.channel.send(message.author.mention +"  "+ str(line))
+
+    
+  if message.content.lower() == "prx paranoia":
+    line=paranoia()
+    await message.channel.send(message.author.mention +"  "+ str(line))
+
+  
+  if message.content.lower() == "xxx truth":
+    line=truthx()
+    await message.channel.send(message.author.mention +"  "+ str(line))
+
+
+  
+  if message.content.lower() == "xxx dare":
+    if str(message.author)=="P R X#5546":
+      await message.channel.send(message.author.mention +"  "+random.choice(dares))
+    else:
+      await message.channel.send(message.author.mention +"  "+random.choice(darez))     
+    #line=darex()
+    #await message.channel.send(message.author.mention +"  "+ str(line))
+
+
+  if message.content.lower() == "xxx never":
+    line=never_have_ix()
+    await message.channel.send(message.author.mention +"  "+ str(line))
+
+
+  if message.content.lower() == "xxx wyr":
+    line=would_you_ratherx()
+    await message.channel.send(message.author.mention +"  "+ str(line))
+
+    
+  if message.content.lower() == "xxx paranoia":
+    line=paranoiax()
+    await message.channel.send(message.author.mention +"  "+ str(line))
+
+
+keep_alive()
+
+client.run(os.environ["7d4797f52db6b151290419f6c7c3c4021feaa34d33eb2ac05e189d7633fcb099"])
